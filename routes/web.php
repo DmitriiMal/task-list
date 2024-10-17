4c111fd60d6e252dae0952e2160f2c96572647f1
Dmitrii Malyshkin <d.w.malyschkin@gmail.com>
2024-10-17 16:49:59 +0200

    added validation and store data from the form in the db

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Task;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
@@ -11,20 +12,32 @@ Route::get('/', function () {
 
 Route::get('/tasks', function () {
     return view('index', [
-        'tasks' => \App\Models\Task::latest()->where('completed', true)->get(),
+        'tasks' => Task::latest()->where('completed', true)->get(),
     ]);
 })->name('tasks.index');
 
 Route::view('/tasks/create', 'create')->name('tasks.create');
 
 Route::get('/tasks/{id}', function ($id) {
-    $task = \App\Models\Task::findOrFail($id);
+    $task = Task::findOrFail($id);
 
     return view('show', ['task' => $task]);
 })->name('tasks.show');
 
 Route::post('/task', function (Request $request) {
-    dd($request->all());
+    $data = $request->validate([
+        'title' => 'required|max:255',
+        'description' => 'required',
+        'long_description' => 'required'
+    ]);
+
+    $task = new Task;
+    $task->title = $data['title'];
+    $task->description = $data['description'];
+    $task->long_description = $data['long_description'];
+
+    $task->save();
+    return redirect()->route('tasks.show', ['id' => $task->id]);
 })->name('tasks.store');
 
 
